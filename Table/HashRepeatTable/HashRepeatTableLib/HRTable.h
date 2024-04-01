90fba52c0182518f2aea7c1647d074853de9f5af
thunderbird <greg.varfolomeev@yandex.ru>
2024-04-01 16:38:15 +0300

    HRT, HLT, AOT, ALT, Poli, List

--- Table/HashRepeatTable/HashRepeatTableLib/HRTable.h
+++ Table/HashRepeatTable/HashRepeatTableLib/HRTable.h
@@ -11,6 +11,9 @@ class HashRepeatTable : public Table<Key, Value>
 {
 private:
 	vector<Row> vec;
+	int curs = -1;  // current item
+	int count = 0; // not empty elems counter
+	int steps = -1;
 protected:
 	int HashKey(Key _key) const;
 	int Rehash(Key _key, int seed) const;
@@ -18,6 +21,8 @@ protected:
 	int keyToInt(const Key& k);
 	int simpleHash(Key k);
 public:
+	double Getfullness(void) const;
+	int GetCount(void) const;
 	Value* Find(Key _key) override;
 	int Insert(Key _key, Value _val) override;
 	int Delete(Key _key) override;
@@ -29,7 +34,17 @@ public:
 	Value* GetValuePtr(void) override;
 };
 
+template<class Key, class Value>
+double HashRepeatTable<Key, Value>::Getfullness(void) const
+{
+	return (double)count / size;
+}
 
+template<class Key, class Value>
+int HashRepeatTable<Key, Value>::GetCount(void) const
+{
+	return this->count;
+}
 template<class Key, class Value>
 int HashRepeatTable<Key, Value>::HashKey(Key _key) const
 {
@@ -37,7 +52,7 @@ int HashRepeatTable<Key, Value>::HashKey(Key _key) const
 	string str = to_string(_key);
 	for (int i = 0; i < str.length(); i++)
 		sum += str[i];
-	return sum;
+	return sum % this->maxsize;
 }
 
 template<class Key, class Value>
@@ -46,8 +61,8 @@ int HashRepeatTable<Key, Value>::Rehash(Key _key, int seed) const
 	int sum = seed;
 	string str = to_string(_key);
 	for (int i = 0; i < str.length(); i++)
-		res += str[i];
-	return res %= this->maxsize;
+		sum += str[i];
+	return sum % this->maxsize;
 
 }
 
@@ -55,12 +70,12 @@ template<class Key, class Value>
 Value* HashRepeatTable<Key, Value>::Find(Key _key)
 {
 	if (this->IsEmpty()) return nullptr;
-	int hk = this->HashKey(_key);
-	for (int i = 0; i < vec.size(); i++) {
-		if (vec[i].key == hk)
-			return &(vec[i].value);
-	}
-	return nullptr;
+	int id = this->HashKey(_key);
+	while (vec[id].key != _key && vec[id].key != Key())
+		id = (id + 1) % this->maxsize;
+
+	if (vec.size() <= id || vec[id].key == Key()) return nullptr;
+	return &vec[id].value;
 }
 
 
@@ -68,56 +83,85 @@ template<class Key, class Value>
 int HashRepeatTable<Key, Value>::Insert(Key _key, Value _val)
 {
 	if (this->IsFull()) return -1;
-	int hk = this->HashKey(_key);
-	if (this->Find(hk) != nullptr) {
-		hk = this->Rehash(_key, 11);
+
+	int id = this->HashKey(_key);
+	int repeat_id = -1;
+	
+	if (vec.size() <= id){
+		if ((id + 1) != this->maxsize) {
+			vec.resize(id);
+			vec.push_back({ _key, _val });
+			count++;
+			size = vec.size();
+			if (count == 1) {
+				//curs++;
+				this->Reset();
+			}
+			return 0;
+		}
+		else return -1; // reached maxSize
+		
 	}
-	vec.push_back({ hk ,_val });
-	size++;
+	while (vec[id].key != Key()) {
+		id = (id + 1) % this->maxsize;
+	}
+	if (vec[id].key == Key()) {
+		repeat_id = id;
+	}
+	if (repeat_id != -1) {
+		vec[repeat_id] = Row{ _key, _val };
+	}
+	else {
+		vec[id].value = _val;
+	}
+	count++; // plus one new elem
 	return 0;
 }
 
 template<class Key, class Value>
 int HashRepeatTable<Key, Value>::Delete(Key _key)
 {
-	
+	if (this->IsEmpty()) return - 1;
+	if (this->Find(_key) == nullptr) return -1;
+	int id = this->HashKey(_key);
+	vec[id] = Row{ Key(),Value() };
 	return 0;
-
 }
 
 template<class Key, class Value>
 void HashRepeatTable<Key, Value>::Reset(void)
 {
-	return;
-
+	steps = 0;
+	curs = -1;
+	this->GoNext();
 }
 
 template<class Key, class Value>
 bool HashRepeatTable<Key, Value>::IsTabEnded(void) const
 {
-	return true;
-
+	return steps == count + 1;
 }
 
 template<class Key, class Value>
 int HashRepeatTable<Key, Value>::GoNext(void)
 {
-	return 1;
+	do { curs = (curs + 1) % size; }
+	while (vec[curs].key == Key());
 
+	if (count != 0 && vec[curs].key != Key())
+		steps++;
+	return 0;
 }
 
 template<class Key, class Value>
 Key HashRepeatTable<Key, Value>::GetKey(void) const
 {
-	int hk = this->HashKey(0);
-	return NULL;
-
+	return (curs != -1) ? vec[curs].key : NULL;
 }
 
 template<class Key, class Value>
 Value* HashRepeatTable<Key, Value>::GetValuePtr(void)
-{
-	int hk = this->HashKey(0);
-	return nullptr;
+{ 
+	return (curs != -1) ? &vec[curs].value : nullptr;
 
 }
--- Table/HashRepeatTable/main.cpp
+++ Table/HashRepeatTable/main.cpp
@@ -5,7 +5,25 @@
 using namespace std;
 int main() {
 	HashRepeatTable<int, std::string> t;
-	t.Insert(12, "stupid table");
-	t.Insert(1, "good table");
+	t.Insert(12, "(12 good table)");//
+	t.Insert(32, "(32 good table)");//
+	t.Insert(45, "(45 good table)");//
+	t.Insert(12, "(12 good table)");
+	t.Insert(2, "(2 good table)");//
+	t.Insert(1, "(1 good table)");
+	t.Insert(1, "(1 good table)");
+	t.Insert(1, "(1 good table)");
+	t.Insert(1, "(1 good table)");
+	t.Insert(1, "(1 good table)");
+	t.Insert(1, "(1 good table)");
+	t.Insert(1, "(1 good table)");
+	t.Insert(1, "(1 good table)");
+	cout << t << " " << t.GetSize() << " " << t.GetSize() / 10000.0 << endl << t.Getfullness() << " " << t.GetCount() / (double)t.GetSize() << endl;
+
+	t.Delete(1);
+	t.Delete(45);
+	cout << t << endl;
+
+	
 	return 0;
 }
